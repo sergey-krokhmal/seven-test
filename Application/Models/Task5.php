cff1af4a9cbaf042ffc79df9f89182c3c6eba529
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-07-06 15:20:59 +0300

    Add Task5 model

---
+++ Application/Models/Task5.php
@@ -0,0 +1,37 @@
+<?php
+namespace Application\Models;
+
+class Task5
+{
+    private $db;
+	private $rows;
+    
+    public function __construct($db)
+    {
+		$this->db = $db;
+		$task3 = new Task3($db);
+		$this->row = $task3->getInputData();
+    }
+    
+    public function getInputData()
+    {
+        return $this->row;
+    }
+    
+    public function getResult()
+    {
+        $stm = $db->query("
+			SELECT a.* FROM `departs` a
+			LEFT JOIN `departs` b ON a.parent = b.id
+			WHERE b.id IS NULL AND a.id IN (
+				SELECT b.parent
+				FROM `departs` a
+				LEFT JOIN `departs` b ON a.id = b.parent
+				GROUP BY b.parent
+				HAVING COUNT(b.id) >= 3
+			)
+		");
+
+		return $stm->fetchAll();
+	}
+}
