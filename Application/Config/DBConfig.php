6256023704039708b47df7fcb98c886ce081c6c0
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-07-07 23:35:27 +0300

    Add end lines

--- Application/Config/DBConfig.php
+++ Application/Config/DBConfig.php
@@ -1,7 +1,12 @@
 <?php
 namespace Application\Config;
 
+// Конфигурация подключения к БД
 class DBConfig
 {
+    const HOST = "localhost";   // хост БД
+    const USER = "tree";        // Пользователь
+    const PASS = "tree";        // Пароль
+    const DB_NAME = "tree";     // Имя БД
 }
  
